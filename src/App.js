31660c2042ce5684b47faf50ba70be564b6e6b21
André Teixeira Silva <andreteixeirasilva@Mac-mini-de-Andre.local>
2025-07-04 13:20:29 -0300

    change home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <img src="/snake.png" alt="Apolo Logo" className="logo" />
       <h1>MAFIA SEASON 2026</h1>
+      <h6>HE'S WATCHING US RIGHT NOW.</h6>
     </div>
   );
 }
